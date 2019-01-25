880bd4b47167d7646c3fce074eb99ad65eabe27c
Mpv1983 <46942529+Mpv1983@users.noreply.github.com>
2019-01-25 14:57:12 +0000

    Number Sorter Project Added

---
+++ NumberSorter/Sorter.cs
@@ -0,0 +1,37 @@
+﻿//***************************************************************************************
+//                          Written By Mike Valchera
+//***************************************************************************************
+
+using INumberSorter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberSorter
+{
+    /// <summary>
+    /// This class implements ISorter and will return numbers in a sorted manor
+    /// </summary>
+    public class Sorter : ISorter
+    {
+        /// <summary>
+        /// Sorts numbers from Highest to lowest
+        /// </summary>
+        public List<int> SortHighToLow(string inputValues)
+        {
+            if(inputValues == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if(!int.TryParse(inputValues, out int n)){
+                throw new ArgumentException("Input value must be a number");
+            }
+
+            List<int> returnList = inputValues.Select(x => x.ToString()).ToArray().Select(Int32.Parse).ToList();
+
+            return returnList.OrderByDescending(x => x).ToList();
+
+        }
+    }
+}
