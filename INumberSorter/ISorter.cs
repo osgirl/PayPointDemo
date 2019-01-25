b78efe12230814fa84db766866aad8d13c41df8b
Mpv1983 <46942529+Mpv1983@users.noreply.github.com>
2019-01-25 14:55:02 +0000

    Interface project added

---
+++ INumberSorter/ISorter.cs
@@ -0,0 +1,16 @@
+﻿//***************************************************************************************
+//                          Written By Mike Valchera
+//***************************************************************************************
+
+using System.Collections.Generic;
+
+namespace INumberSorter
+{
+    /// <summary>
+    /// Interface to Number sorter
+    /// </summary>
+    public interface ISorter
+    {
+        List<int> SortHighToLow(string inputValues);
+    }
+}
